d757c8a85615fc20b349e9876bb56f461157e825
AlIbay <43456952+AlIbay8@users.noreply.github.com>
2024-05-11 11:23:48 -0700

    consistent casing

--- profile/script.js
+++ profile/script.js
@@ -1,28 +1,39 @@
 document.addEventListener("DOMContentLoaded", (event) => {
     console.log("DOM fully loaded and parsed");
-    const saveChangeBtn = document.getElementById("submit-btn");
-    const nameLabel = document.getElementById("name");
-    const emailLabel = document.getElementById("email");
-    const bioLabel = document.getElementById("bio");
-    const nameInput = document.getElementById("nameInput");
-    const emailInput = document.getElementById("emailInput");
-    const bioInput = document.getElementById("bioInput");
-    
+    const save_changes_btn = document.getElementById("submitBtn");
+    const name_label = document.getElementById("name");
+    const email_label = document.getElementById("email");
+    const bio_label = document.getElementById("bio");
+    const pfp_display = document.getElementById("pfpDisplay")
+    const name_input = document.getElementById("nameInput");
+    const email_input = document.getElementById("emailInput");
+    const bio_input = document.getElementById("bioInput");
+    const pfp_input = document.getElementById("pfpInput");
 
     function getNewInfo() {
         let input_info = {
-            "name": nameInput.value,
-            "email": emailInput.value,
-            "bio": bioInput.value
+            "name": name_input.value,
+            "email": email_input.value,
+            "bio": bio_input.value,
+            "pfp": ""
         }
-        console.log(input_info)
+        let image = pfp_input.files[0];
+        //const Get
+        let reader = new FileReader();
+        reader.onload = function(e) {
+            input_info.pfp = e.target.result;
+            console.log(pfp_input.files[0])
+            console.log(e.target.result)
+        }
+        reader.readAsDataURL(image);
         return input_info
     }
 
-    saveChangeBtn.addEventListener("click", function() {
-        updated_info = getNewInfo()
-        nameLabel.innerText = updated_info.name
-        emailLabel.innerText = updated_info.email
-        bioLabel.innerText = updated_info.bio
+    save_changes_btn.addEventListener("click", function() {
+        updated_info = getNewInfo();
+        pfp_display.src = updated_info.pfp
+        name_label.innerText = updated_info.name
+        email_label.innerText = updated_info.email
+        bio_label.innerText = updated_info.bio
     })
 });
