9b3db71c853fbd86951e403901007e65f0fee01d
AlIbay <43456952+AlIbay8@users.noreply.github.com>
2024-05-18 16:40:23 -0700

    fix server message

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.get('/product/:id', (req, res) => {
 
 const port = process.env.PORT || 3001;
 const server = app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
 
 server.keepAliveTimeout = 120 * 1000;
