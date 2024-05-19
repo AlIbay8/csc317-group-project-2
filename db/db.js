fad9d0f1cb37c637a0738461dc285d37dc12b6ec
AlIbay <43456952+AlIbay8@users.noreply.github.com>
2024-05-18 21:02:54 -0700

    rebranding

--- db/db.js
+++ db/db.js
@@ -16,7 +16,8 @@ db.serialize(() => {
         genre VARCHAR,
         price FLOAT(6,2),
         img VARCHAR,
-        desc TEXT
+        desc TEXT,
+        date_published DATE
     )`);
     db.run(`CREATE TABLE IF NOT EXISTS cart_items (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,31 +33,35 @@ function prepareProducts() {
     const products = [
         {
             name: "The 8th Hour",
-            genre: "adventure,puzzle",
+            genre: "adventure, puzzle",
             price: 7.99,
             img: "/resources/T8H-thumbnail-small.gif",
-            desc: "test"
+            date_published: '2024-03-24',
+            desc: "A time-looping text adventure. Explore an abandoned village. You wake up in an apartment of a deserted village. As you are exploring, you find the community which was once here referred to themselves as \"The Order of Perennial Mortals.\" Find the answers. Learn the secrets of the village and its residents. Follow the clues to search for the source of your time loop. Break the loop. \"What is called a reason for living is also an excellent reason for dying.\""
         },
         {
             name: "What the Curl?",
-            genre: "sports,comedy",
+            genre: "sports, comedy",
             price: 4.99,
             img: "/resources/WTC_Thumbnail.gif",
-            desc: "test"
+            date_published: '2023-05-21',
+            desc: "WHAT THE CURL? is a curling simulation that is anything but curling. Much like its inspiration, WHAT THE GOLF? by Triband, it is a comedy game with some light puzzles and a few surprises. Wield your mighty brush to control the stone's fate. Evade your opponent's stones, ride some waves, partake in a coup d'état, and more in WHAT THE CURL? Master your curling skills in a tutorial, 4 levels, 12 gags/sublevels, and a boss fight."
         },
         {
             name: "The Nature of Gravity",
-            genre: "platformer,puzzle",
+            genre: "platformer, puzzle",
             price: 5.99,
             img: "/resources/TNOG_Thumbnail.gif",
-            desc: "test"
+            date_published: '2022-08-18',
+            desc: "Navigate through obstacles and deadly objects using your unique platforming ability: influencing the direction of gravity. Collect arrow keys, jump over spikes, move crates, and reach the flag at the end of the level by understanding the nature of gravity."
         },
         {
             name: "Telepathic Puzzler",
             genre: "puzzle",
             price: 1.99,
             img: "/resources/TelepathicPuzzler_Thumbnail.gif",
-            desc: "test"
+            date_published: '2021-06-13',
+            desc: "Play as the two telepathically connected puzzle pieces, Poly and Beta, in their journey to join together! The goal is to get Poly and Beta to join together by maneuvering through obstacles and understanding the telepathic controls."
         }
     ]
     for (let product of products) {
@@ -67,7 +72,7 @@ function prepareProducts() {
                 console.log("Error getting row: ", err)
             }
             if (!row) {
-                db.run(`INSERT INTO products (${keys.join(", ")}) VALUES (?, ?, ?, ?, ?)`, values, (err) => {
+                db.run(`INSERT INTO products (${keys.join(", ")}) VALUES (?, ?, ?, ?, ?, ?)`, values, (err) => {
                     if (err) {
                         console.log(err)
                         return
